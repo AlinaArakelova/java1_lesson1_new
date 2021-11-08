869f8d878c07cba0dc33c3fdc3e22db23382e888
AlinaArakelova <danikalina@gmail.com>
2021-11-08 19:13:16 +0300

    lesson1

--- src/main/java/com/geekbrains/main.java
+++ src/main/java/com/geekbrains/main.java
@@ -1,5 +1,50 @@
 package com.geekbrains;
 
 public class main {
-   
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign(5, 10);
+        printColor();
+        compareNumbers();
+    }
+
+
+    private static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+
+    }
+
+    private static void checkSumSign(int a, int b) {
+        int sum = a + b;
+        if (a + b >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    private static void printColor() {
+        int value = 106;
+        if (value <= 0) {
+            System.out.println("Красный");
+        }
+        if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        }
+        if (value > 100) {
+            System.out.println("Зеленый");
+        }
+    }
+
+    private static void compareNumbers() {
+        int a = 10;
+        int b = 15;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
 }
